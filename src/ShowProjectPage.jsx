d9b45fafe790d93a15683f816f86d9ec1be08220
lelele <aleshkaserb2003@gmail.com>
2024-02-10 22:38:46 +0200

    fix

--- src/ShowProjectPage.jsx
+++ src/ShowProjectPage.jsx
@@ -13,55 +13,57 @@ function ShowProjectPage({ activeNavId, handleNavClick }) {
   handleNavClick("");
 
   return (
-    <div>
+    <>
       <PopupGallery
         projectsInfo={projectsInfo}
         currentProject={currentProject}
         isShowPopup={showPopupGallery}
         setShowPopup={setShowPopupGallery}
       />
-      <header>
-        <div className="logo">
-          <img src={logo} />
-        </div>
+      <div>
+        <header>
+          <div className="logo">
+            <img src={logo} />
+          </div>
 
-        <nav className="phoneNav">
-          <Link to={"/"}>
-            <span
-              id="navMain"
-              className={
-                activeNavId === "navMain" ? "navSpan active" : "navSpan"
-              }
-              onClick={() => handleNavClick("navMain")}
-            >
-              Головна
-            </span>
-          </Link>
-          <Link to={"/portfolio"}>
-            {" "}
-            <span
-              id="navPortfolio"
-              className={
-                activeNavId === "navPortfolio" ? "navSpan active" : "navSpan"
-              }
-              onClick={() => handleNavClick("navPortfolio")}
-            >
-              Портфоліо
-            </span>
-          </Link>
-        </nav>
-      </header>
+          <nav className="phoneNav">
+            <Link to={"/"}>
+              <span
+                id="navMain"
+                className={
+                  activeNavId === "navMain" ? "navSpan active" : "navSpan"
+                }
+                onClick={() => handleNavClick("navMain")}
+              >
+                Головна
+              </span>
+            </Link>
+            <Link to={"/portfolio"}>
+              {" "}
+              <span
+                id="navPortfolio"
+                className={
+                  activeNavId === "navPortfolio" ? "navSpan active" : "navSpan"
+                }
+                onClick={() => handleNavClick("navPortfolio")}
+              >
+                Портфоліо
+              </span>
+            </Link>
+          </nav>
+        </header>
 
-      <MySlider
-        setShowPopupGallery={setShowPopupGallery}
-        // setActivePortf={handleNavClick}
-        slides={projectsInfo}
-        currentProject={currentProject}
-        parentWidth={440}
-        // openedPhoneSlider={openedPhoneSlider}
-        // setOpenedPhoneSlider={setOpenedPhoneSlider}
-      />
-    </div>
+        <MySlider
+          setShowPopupGallery={setShowPopupGallery}
+          // setActivePortf={handleNavClick}
+          slides={projectsInfo}
+          currentProject={currentProject}
+          parentWidth={440}
+          // openedPhoneSlider={openedPhoneSlider}
+          // setOpenedPhoneSlider={setOpenedPhoneSlider}
+        />
+      </div>
+    </>
   );
 }
 
